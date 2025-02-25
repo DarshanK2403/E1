cb1c63a05fb40040e8024fc04d0b1aa81bcc57cf
DarshanK2403 <darshandeesa2403@gmail.com>
2025-02-25 13:49:20 +0530

    Model

--- index.js
+++ index.js
@@ -2,11 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
-const userRoute = require('./src/routes/userRoute')
+const userRoute = require("./src/routes/userRoute");
 
-// Use the router for all routes starting with '/'
-app.use('/', userRoute);
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb://localhost:27017/E_Com", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database Connected");
+  })
+  .catch((err) => {
+    console.error("Database Connection Error: ", err);
+  });
+
+app.use("/", userRoute);
 
 app.listen(PORT, () => {
-    console.log(`server running http://localhost:4000`);
+  console.log(`server running http://localhost:4000`);
 });
---
+++ src/model/UserModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Define the User schema
+const UserSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  dateJoined: {
+    type: Date,
+    default: Date.now
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user'
+  }
+});
+
+// Create the User model
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
